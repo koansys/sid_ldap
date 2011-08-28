f061e0d66c734bf5e74fcd132d6a622dfde83026
Chris Shenton <chris@koansys.com>
2011-08-28 17:54:23 -0400

    Get LDAP slapd running with sample users

--- manifests/sid_ldap.pp
+++ manifests/sid_ldap.pp
@@ -2,7 +2,17 @@
 # Want trac, httpd, ldap.
 # later various ldap apache modules, configs
 
-# TODO: wrap in "class lucid32" equivalent.
+# TODO:
+# - wrap in platform specific class, like "class lucid32 {..."
+# - use SSL cert for LDAPS
+# - use client's real basedn
+# - see for parametric trac config generation: http://docs.puppetlabs.com/guides/templating.html
+#
+# NOTES:
+# - create rootpw with 'slappasswd'
+# - create users and passwords with ... what?
+###############################################################################
+
 
 # Run 'facter' on VM to find the facts puppet knows; samples:
 file { vars:
@@ -77,3 +87,35 @@ service { httpd:
   ensure => running,
 }
 
+# process is called 'slapd' but we need its /etc/init.d/ldap name
+service { ldap:
+  ensure        => running,
+  enable        => true,
+  subscribe     => File['slapd.conf']
+}
+
+# Centos runs this as user 'ldap' group 'ldap'
+# The source relies on Vagrant mounting its dir on target as /vagrant.
+file { 'slapd.conf':
+  name          => '/etc/openldap/slapd.conf',
+  ensure        => present,
+  source        => '/vagrant/files/slapd.conf',
+  owner         => root,
+  group         => ldap,
+  mode          => 0640,
+}
+
+# Add initial organization and test users.
+# I believe we need to run as root to use 'slapcat'.
+# TODO: require service ldap/slapd
+
+exec { 'ldapaddusers':
+  path          => ['/bin', '/usr/bin', '/usr/sbin'],
+  command       => "ldapadd -x -D 'cn=manager,dc=example,dc=gov' -w password -f /vagrant/files/users.ldif",
+  cwd           => "/vagrant/files",
+  logoutput     => true,
+  unless        => ['slapcat | grep "dn: ou=People,dc=example,dc=gov"',
+                    'slapcat | grep "dn: uid=user1,ou=People,dc=example,dc=gov"',
+                    'slapcat | grep "dn: uid=user1,ou=People,dc=example,dc=gov"',
+                    ],
+}
