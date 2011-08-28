d3e22238f84d03e133e52fdf4573e5cb53b59bd5
Chris Shenton <chris@koansys.com>
2011-08-28 15:51:48 -0400

    Make packages depend on platform (centos), add ldap services

--- manifests/sid_ldap.pp
+++ manifests/sid_ldap.pp
@@ -4,18 +4,76 @@
 
 # TODO: wrap in "class lucid32" equivalent.
 
-package { httpd:
-        ensure => latest;
+# Run 'facter' on VM to find the facts puppet knows; samples:
+file { vars:
+  ensure => file,
+  path => "/tmp/vars",
+  mode => 0644,
+  content => "hostname=${::hostname} operatingsystem=${::operatingsystem} fqdn=${::fqdn} domain=${::domain} ipaddress=${::ipaddress}",
 }
 
-package { trac:
-        ensure => installed;
+$apache = $operatingsystem ? {
+  /(?i)(centos|redhat)/ => 'httpd',
+  /(?i)(ubuntu|debian)/ => "apache2",
+  default               => undef,
 }
 
-package { "emacs-nox":
-  ensure => latest;
+$emacs = $operatingsystem ? {
+  /(?i)(centos|redhat)/ => 'emacs-nox',
+  /(?i)(ubuntu|debian)/ => "emacs-nox11",
+  default               => undef,
 }
+
+$ldapdev = $operatingsystem ? {
+  /(?i)(centos|redhat)/ => 'openldap-devel',
+  /(?i)(ubuntu|debian)/ => "openldap-devel",
+  default               => undef,
+}
+
+$ldapclients = $operatingsystem ? {
+  /(?i)(centos|redhat)/ => 'openldap-clients',
+  /(?i)(ubuntu|debian)/ => "openldap-clients",
+  default               => undef,
+}
+
+$ldapservers = $operatingsystem ? {
+  /(?i)(centos|redhat)/ => 'openldap-servers',
+  /(?i)(ubuntu|debian)/ => "openldap-servers",
+  default               => undef,
+}
+
+package { apache:
+  name          => $apache,
+  ensure        => latest;
+}
+
+package { emacs:
+  name          => $emacs,
+  ensure        => latest;
+}
+
+package { ldapdev:
+  name          => $ldapdev,
+  ensure        => latest;
+}
+
+package { ldapclients:
+  name          => $ldapclients,
+  ensure        => latest;
+}
+
+package { ldapservers:
+  name          => $ldapservers,
+  ensure        => latest;
+}
+
+
+# No Centos Trac pkg? "sudo easy_install Trac"?
+#package { trac:
+#  ensure => installed;
+#}
+
 service { httpd:
-        ensure => running,
+  ensure => running,
 }
 
